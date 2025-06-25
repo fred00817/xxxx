9db1cbb915c468d4114c23298b11bdce84ba760b
MUCYO Fred <mucyofred00@gmail.com>
2025-06-25 10:23:53 +0200

    all numbers are squared

--- Squaring.js
+++ Squaring.js
@@ -3,14 +3,18 @@ function squareDigits(num){
     // const square = num.map((item) => item ^2)
     // return square
     let str = String(num)
-    let joiin = str.split('').join(",")
-    let pow = parseInt(joiin)
-
-    ///const squarii = pow.map((item)=> item*item)
+    let joiin = str.split('').join("")
     
-    return pow 
+    
+let squaree = ''
+    //const squarii = arr.map((item)=> item*item)
+    for(let i=0;i<joiin.length;i++){
+     squaree += joiin[i]*joiin[i]
+    }
+    
+    return squaree
 }
 
 
 
-  console.log(squareDigits(34))
+  console.log(squareDigits(343333))
